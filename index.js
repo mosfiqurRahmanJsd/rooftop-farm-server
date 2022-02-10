271dd5a7938306c50189c243ba6e7fe66ea8f2db
mosfiqurRahmanJsd <nisho.mushfiq2020@gmail.com>
2022-02-11 05:18:57 +0600

    product added

--- index.js
+++ index.js
@@ -7,18 +7,36 @@ const app = express()
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gkmic.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-async function run(){
-  try{
+async function run() {
+  try {
     await client.connect();
-    console.log('database connected');
+    const database = client.db('rooftop'); 
+    const productCollection = database.collection('product');
+
+    // GET API
+    app.get('/product', async (req, res) => {
+      const cursor = productCollection.find({});
+      const product = await cursor.toArray();
+      res.send(product);
+
+    })
+
+    // POST API 
+    app.post('/product', async(req, res) => {
+      const newProduct = req.body;
+      const result = await productCollection.insertOne(newProduct);
+      console.log('got new product', req.body);
+      console.log('added product', result)
+      res.json(result);
+    })
   }
-  finally{
+  finally {
     // await client.close();
   }
 }
