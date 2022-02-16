e77ee09d57bb1ca009ac27bb55fcce029276b112
mosfiqurRahmanJsd <nisho.mushfiq2020@gmail.com>
2022-02-17 00:46:03 +0600

    backend final

--- index.js
+++ index.js
@@ -11,7 +11,15 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gkmic.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.quauy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
